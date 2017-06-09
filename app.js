8d2878b3854f8cc2c03f11b94dcddda2244ce282
Nilson <molinanilson@gmail.com>
2017-06-09 04:15:21 -0400

    redesigned app

--- app.js
+++ app.js
@@ -5,18 +5,26 @@ var LoanController = (function() {
         loanCost: 0
     };
 
+    var input = {
+        loan: 0,
+        rate: 0,
+        term: 0
+    };
+
+    var table = document.getElementById('resultsTable');
+
     var bindEvents = function() {
         document.getElementById('button').addEventListener('click', calculateLoan);
     };
 
     var calculateLoan = function() {
-        var loan = parseFloat(document.getElementById('principal').value);
-        var rate = parseFloat(document.getElementById('interest').value);
-        var term = parseFloat(document.getElementById('time').value);
+        input.loan = parseFloat(document.getElementById('principal').value);
+        input.rate = parseFloat(document.getElementById('interest').value);
+        input.term = parseFloat(document.getElementById('time').value);
 
-        data.monthlyPayments = getMonthlyPayments(loan, rate, term);
-        data.totalAmount = data.monthlyPayments * term;
-        data.loanCost = data.totalAmount - loan;
+        data.monthlyPayments = getMonthlyPayments(input.loan, input.rate, input.term);
+        data.totalAmount = data.monthlyPayments * input.term;
+        data.loanCost = data.totalAmount - input.loan;
 
         showResults();
     };
@@ -31,9 +39,17 @@ var LoanController = (function() {
         console.log('totalAmount:', data.totalAmount);
         console.log('cost:', data.loanCost);
 
-        document.getElementById('monthly').textContent  = '$' + data.monthlyPayments.toFixed(2);
-        document.getElementById('total').textContent  = '$' + data.totalAmount.toFixed(2);
-        document.getElementById('cost').textContent  = '$' + data.loanCost.toFixed(2);
+        var row = table.insertRow(table.rows.length);
+
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+
+        cell1.innerHTML = '$' + input.loan.toFixed(2) + ' @'+ input.rate + '% for ' + input.term + ' months';
+        cell2.innerHTML = '$' + data.monthlyPayments.toFixed(2);
+        cell3.innerHTML = '$' + data.totalAmount.toFixed(2);
+        cell4.innerHTML = '$' + data.loanCost.toFixed(2);
     };
 
     return {
