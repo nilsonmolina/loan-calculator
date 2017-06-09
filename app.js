b7fbc21898421c552d531566c60cfb5e66d9f3a6
Nilson <MolinaNilson@gmail.com>
2017-06-09 14:02:59 -0400

    made a few style and ui improvements

--- app.js
+++ app.js
@@ -29,27 +29,32 @@ var LoanController = (function() {
         showResults();
     };
 
-    var getMonthlyPayments = function (principal, rate, numberOfPayments) {
+    var getMonthlyPayments = function(principal, rate, numberOfPayments) {
         var effectiveRate = (rate / 100) / 12;
         return principal * (effectiveRate / (1 - Math.pow(1 + effectiveRate, -numberOfPayments)));
     };
 
     var showResults = function() {
-        console.log('monthlyPayments:', data.monthlyPayments);
-        console.log('totalAmount:', data.totalAmount);
-        console.log('cost:', data.loanCost);
-
-        var row = table.insertRow(table.rows.length);
+      // var row = table.insertRow(table.rows.length);
+        var row = table.insertRow(1);
 
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
 
-        cell1.innerHTML = '$' + input.loan.toFixed(2) + ' @'+ input.rate + '% for ' + input.term + ' months';
-        cell2.innerHTML = '$' + data.monthlyPayments.toFixed(2);
-        cell3.innerHTML = '$' + data.totalAmount.toFixed(2);
-        cell4.innerHTML = '$' + data.loanCost.toFixed(2);
+        cell1.innerHTML = parseCurrency(input.loan) + ' @'+ input.rate + '% for ' + input.term + ' months';
+        cell2.innerHTML = parseCurrency(data.monthlyPayments);
+        cell3.innerHTML = parseCurrency(data.totalAmount);
+        cell4.innerHTML = parseCurrency(data.loanCost);
+    };
+
+    var parseCurrency = function(value) {
+      return parseFloat(value.toFixed(2)).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+      });
     };
 
     return {
