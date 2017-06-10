5c519652edaad9a04987338176a16433eae73e6c
Nilson <MolinaNilson@gmail.com>
2017-06-10 00:07:26 -0400

    added validation and restructured javascript code

--- app.js
+++ app.js
@@ -1,71 +1,126 @@
 var LoanController = (function() {
-    var data = {
-        monthlyPayments: 0,
-        totalAmount: 0,
-        loanCost: 0
-    };
+  var data = {
+    monthlyPayments: 0,
+    totalAmount: 0,
+    loanCost: 0,
+    loan: 0,
+    rate: 0,
+    term: 0
+  };
+  var dom = {
+    table: document.getElementById('resultsTable'),
+    loan: document.getElementById('principal'),
+    rate: document.getElementById('interest'),
+    term: document.getElementById('time'),
+    calculatorDiv: document.querySelector('.calculator'),
+    button: document.getElementById('button')
+  };
 
-    var input = {
-        loan: 0,
-        rate: 0,
-        term: 0
-    };
+  var bindEvents = function() {
+    dom.button.addEventListener('click', function() {
+      getUserInput();
+      // Check for errors.
+      if (document.querySelector('.error') !== null) {
+        return;
+      }
+      calculateLoan();
+      outputResults();
+      dom.loan.select();
+    });
 
-    var table = document.getElementById('resultsTable');
+    dom.loan.addEventListener('focusout', function() {
+      isNaN(parseFloat(dom.loan.value)) ? dom.loan.classList.add('error') : dom.loan.classList.remove('error');
+    });
 
-    var bindEvents = function() {
-        document.getElementById('button').addEventListener('click', calculateLoan);
-    };
+    dom.rate.addEventListener('focusout', function() {
+      isNaN(parseFloat(dom.rate.value)) ? dom.rate.classList.add('error') : dom.rate.classList.remove('error');
+    });
 
-    var calculateLoan = function() {
-        input.loan = parseFloat(document.getElementById('principal').value);
-        input.rate = parseFloat(document.getElementById('interest').value);
-        input.term = parseFloat(document.getElementById('time').value);
+    dom.term.addEventListener('focusout', function() {
+      isNaN(parseFloat(dom.term.value)) ? dom.term.classList.add('error') : dom.term.classList.remove('error');
+    });
 
-        data.monthlyPayments = getMonthlyPayments(input.loan, input.rate, input.term);
-        data.totalAmount = data.monthlyPayments * input.term;
-        data.loanCost = data.totalAmount - input.loan;
+    dom.loan.addEventListener('keyup', checkIfEnter);
+    dom.rate.addEventListener('keyup', checkIfEnter);
+    dom.term.addEventListener('keyup', checkIfEnter);
 
-        showResults();
-    };
+  };
 
-    var getMonthlyPayments = function(principal, rate, numberOfPayments) {
-        var effectiveRate = (rate / 100) / 12;
-        return principal * (effectiveRate / (1 - Math.pow(1 + effectiveRate, -numberOfPayments)));
-    };
+  var getUserInput = function() {
+    clearErrors();
+    data.loan = parseFloat(dom.loan.value);
+    data.rate = parseFloat(dom.rate.value);
+    data.term = parseFloat(dom.term.value);
+    validateData();
+  };
 
-    var showResults = function() {
-      // var row = table.insertRow(table.rows.length);
-        var row = table.insertRow(1);
+  var validateData = function() {
+    if (isNaN(data.term)){
+      dom.term.classList.add('error');
+      dom.term.select();
+    } if (isNaN(data.rate)){
+      dom.rate.classList.add('error');
+      dom.rate.select();
+    } if (isNaN(data.loan)){
+      dom.loan.classList.add('error');
+      dom.loan.select();
+    }
+  };
 
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
+  var clearErrors = function() {
+    dom.loan.classList.remove('error');
+    dom.rate.classList.remove('error');
+    dom.term.classList.remove('error');
+  };
 
-        cell1.innerHTML = parseCurrency(input.loan) + ' @'+ input.rate + '% for ' + input.term + ' months';
-        cell2.innerHTML = parseCurrency(data.monthlyPayments);
-        cell3.innerHTML = parseCurrency(data.totalAmount);
-        cell4.innerHTML = parseCurrency(data.loanCost);
-    };
+  var calculateLoan = function() {
+    data.monthlyPayments = getMonthlyPayments(data.loan, data.rate, data.term);
+    data.totalAmount = data.monthlyPayments * data.term;
+    data.loanCost = data.totalAmount - data.loan;
+  }
 
-    var parseCurrency = function(value) {
-      return parseFloat(value.toFixed(2)).toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2,
-      });
-    };
+  var getMonthlyPayments = function(principal, rate, numberOfPayments) {
+    var effectiveRate = (rate / 100) / 12;
+    return principal * (effectiveRate / (1 - Math.pow(1 + effectiveRate, -numberOfPayments)));
+  };
 
-    return {
-        init: function() {
-            bindEvents();
-            setTimeout(function(){
-                document.querySelector('.calculator').classList.toggle('hidden');
-            }, 250);
+  var outputResults = function() {
+    var row = dom.table.insertRow(1); //inserts row at the beginning of the table, after header row
 
-        }
-    };
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+
+    cell1.innerHTML = parseCurrency(data.loan) + ' @'+ data.rate + '% for ' + data.term + ' months';
+    cell2.innerHTML = parseCurrency(data.monthlyPayments);
+    cell3.innerHTML = parseCurrency(data.totalAmount);
+    cell4.innerHTML = parseCurrency(data.loanCost);
+  };
+
+  var parseCurrency = function(value) {
+    return parseFloat(value.toFixed(2)).toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    });
+  };
+
+  var checkIfEnter = function(e) {
+    if(e.keyCode === 13){
+      dom.button.click();
+    }
+  };
+
+  return {
+    init: function() {
+      bindEvents();
+      setTimeout(function(){
+          dom.calculatorDiv.classList.toggle('hidden');
+          dom.loan.select();
+      }, 250);
+    }
+  };
 })();
 
 LoanController.init();
